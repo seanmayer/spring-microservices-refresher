56e84bd1f45ced695a3e3fe37ce7f46110578433
Sean Mayer <apart-things.0p@icloud.com>
2023-01-27 07:12:25 +0000

    Adding Configuration - from application.properties

---
+++ limits-service/src/main/java/com/microservices/limitsservice/configuration/Configuration.java
@@ -0,0 +1,16 @@
+package com.microservices.limitsservice.configuration;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@Component
+@ConfigurationProperties("limits-service")
+public class Configuration {
+
+  private int minimum;
+  private int maximum;
+}
