89b0a4e2345951c7d83ca1b9185a97a448546c8c
Sean Mayer <apart-things.0p@icloud.com>
2023-02-05 19:23:54 +0000

    JPA added repository

--- currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,7 +1,5 @@
 package com.microservices.currencyexchangeservice;
 
-import java.math.BigDecimal;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class CurrencyExchangeController {
 
   @Autowired
+  private CurrencyExchangeRepository repository;
+
+  @Autowired
   private Environment environment;
 
   @GetMapping("/currency-exchange/from/{from}/to/{to}")
@@ -19,12 +20,18 @@ public class CurrencyExchangeController {
     @PathVariable String from,
     @PathVariable String to
   ) {
-    CurrencyExchange currencyExchange = new CurrencyExchange(
-      1000L,
-      from,
-      to,
-      BigDecimal.valueOf(50)
-    );
+    // CurrencyExchange currencyExchange = new CurrencyExchange(
+    //   1000L,
+    //   from,
+    //   to,
+    //   BigDecimal.valueOf(50)
+    // );
+    CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
+    if (currencyExchange == null) {
+      throw new RuntimeException(
+        "Unable to find data for " + from + " to " + to
+      );
+    }
     String port = environment.getProperty("local.server.port");
     currencyExchange.setEnvironment(port);
     return currencyExchange;
