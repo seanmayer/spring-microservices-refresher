b231571b661ac7af13893da181bcabcdbf24ad35
Sean Mayer <apart-things.0p@icloud.com>
2023-02-07 07:41:32 +0000

    Connecting microservices together - Invoking currency exchange service from the currency conversion microservice

--- currency-conversion-service/src/main/java/com/microservices/currencyconversionservice/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -1,9 +1,12 @@
 package com.microservices.currencyconversionservice;
 import java.math.BigDecimal;
+import java.util.HashMap;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CurrencyConversionController {
@@ -14,15 +17,26 @@ public class CurrencyConversionController {
     @PathVariable String to,
     @PathVariable BigDecimal quantity
   ) {
-    CurrencyConversion currencyConversion = new CurrencyConversion(
-      10001L,
+
+    HashMap<String,String> uriVariables = new HashMap<>();
+    uriVariables.put("from",from);
+    uriVariables.put("to",to);
+
+    ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().getForEntity(
+      "http://localhost:8000/currency-exchange/from/{from}/to/{to}",
+      CurrencyConversion.class,uriVariables
+    );
+
+    CurrencyConversion response = responseEntity.getBody();
+
+    return new CurrencyConversion(
+      response.getId(),
       from,
       to,
       quantity,
-      BigDecimal.ONE,
-      BigDecimal.ONE,
-      ""
+      response.getConversionMultiple(),
+      quantity.multiply(response.getConversionMultiple()),
+      response.getEnvironment()
     );
-    return currencyConversion;
   }
 }
