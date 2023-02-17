1be23e06ec2d633e50ebe1dae6fdbd54e75016cb
Sean Mayer <apart-things.0p@icloud.com>
2023-02-17 08:05:46 +0000

    Resilience4J - added more configs

--- currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -1,5 +1,6 @@
 package com.microservices.currencyexchangeservice;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
 
 import org.slf4j.Logger;
@@ -15,7 +16,8 @@ public class CircuitBreakerController {
  private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
 
   @GetMapping("/sample-api")
-  @Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+  //@Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
+  @CircuitBreaker(name = "default", fallbackMethod = "hardcodedResponse")
   public String sampleApi() {
     logger.info("Sample API call received");
     ResponseEntity<String> forEntity = new RestTemplate()
