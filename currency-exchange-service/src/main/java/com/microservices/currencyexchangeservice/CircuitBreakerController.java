d068c31ad5522426b3c447f2fd8b4be003583dd9
Sean Mayer <apart-things.0p@icloud.com>
2023-02-17 07:26:03 +0000

    Resilience4j - retry, fallback configurations

--- currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -1,13 +1,29 @@
 package com.microservices.currencyexchangeservice;
 
+import io.github.resilience4j.retry.annotation.Retry;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CircuitBreakerController {
 
+ private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
+
   @GetMapping("/sample-api")
+  @Retry(name = "sample-api", fallbackMethod = "hardcodedResponse")
   public String sampleApi() {
-    return "Sample API";
+    logger.info("Sample API call received");
+    ResponseEntity<String> forEntity = new RestTemplate()
+      .getForEntity("http://localhost:8080/some-dummy-url", String.class);
+    return forEntity.getBody();
+  }
+
+  public String hardcodedResponse(Exception ex) {
+    return "fallback-response";
   }
 }
