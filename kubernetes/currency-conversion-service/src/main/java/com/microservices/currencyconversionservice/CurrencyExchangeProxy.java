13d140a274a82d129eb566c42381a861e82039a5
Sean Mayer <apart-things.0p@icloud.com>
2023-05-09 21:43:04 +0100

    Kubernetes - formating

--- kubernetes/currency-conversion-service/src/main/java/com/microservices/currencyconversionservice/CurrencyExchangeProxy.java
+++ kubernetes/currency-conversion-service/src/main/java/com/microservices/currencyconversionservice/CurrencyExchangeProxy.java
@@ -4,7 +4,10 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currency-exchange", url = "${CURRENCY_EXCHANGE_SERVICE_HOST:http://localhost}:8000")
+@FeignClient(
+  name = "currency-exchange",
+  url = "${CURRENCY_EXCHANGE_SERVICE_HOST:http://localhost}:8000"
+)
 public interface CurrencyExchangeProxy {
   @GetMapping("/currency-exchange/from/{from}/to/{to}")
   public CurrencyConversion retrieveExchangeValue(
